1a5e267951f31691880ce33b2b69114a0718f655
Jose Rene Correa Padilla <jose.correa@constructorabolivar.com>
2023-07-21 17:33:35 -0500

    Estudio 1

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'blog_database',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 

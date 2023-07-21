eeefac1d70d9675b77b0c5808b2d8b600a465299
Jose Rene Correa Padilla <jose.correa@constructorabolivar.com>
2023-07-21 17:08:21 -0500

    Primer Ejemplo

---
+++ Blog/user/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def Login(request):
+    return HttpResponse("Hola estas en el login")
+def Register(request):
+    return  HttpResponse("Hola estas en el registro")
